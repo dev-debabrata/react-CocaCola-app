1da6953bb535d3c510156aa4c4a8e0ceddd543fb
dev-debabrata <debabratadas711@gmail.com>
2025-06-11 23:55:12 +0530

    11-06-2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Products from './Components/Products'
 
 const App = () => {
   return (
-    <main>
-
+    <main className="overflow-x-hidden">
       <Hero />
+      <Products />
     </main>
   )
 }
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import P1 from "../assets/cocacola1.png";
+import P2 from "../assets/cocacola2.png";
+import P3 from "../assets/cocacola3.png";
+import { motion } from "framer-motion";
+
+const fadeUp = (delay) => {
+  return {
+    hidden: {
+      opacity: 0,
+      y: 100,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        delay: delay,
+      },
+    },
+  };
+};
+
+const ProductsData = [
+  {
+    id: 1,
+    title: "Coca Zero",
+    image: P1,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    delay: 0.5,
+  },
+  {
+    id: 2,
+    title: "Orange Fanta",
+    image: P2,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    delay: 0.8,
+  },
+  {
+    id: 3,
+    title: "Lemon Fanta",
+    image: P3,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    delay: 1.1,
+  },
+];
 
 const Products = () => {
-    return (
-        <div>Products</div>
-    )
-}
+  return (
+    <div className=" bg-gray-100 py-8">
+      <div className=" container py-14">
+        <h1 className="text-3xl font-bold text-center pb-10">Our Products</h1>
+        {/* Card section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {ProductsData.map((item) => (
+            <motion.div className="flex flex-col items-center justify-center p-5 max-w-[300px] mx-auto shadow-lg rounded-xl bg-white">
+              <img
+                src={item.image}
+                alt=""
+                className=" w-[100px] mb-4 hover:rotate-12 hover:scale-110 duration-300"
+              />
+              <div className="text-center space-y-2">
+                <h1 className="text-2xl font-bold font-meri text-center">
+                  {item.title}
+                </h1>
+                <p className=" text-center text-sm text-gray-600">
+                  {item.desc}
+                </p>
+                <button className="!mt-5 border-2 border-paimary text-primary px-6 py-2 rounded-md hover:bg-primary hover:text-white duration-200">
+                  Buy Now
+                </button>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
